81b31b93855792e85ab5705d9070087fa5ff2525
Gonzalo Alejandro Cozzi <gcozzi@axoft.com>
2018-09-03 10:17:23 -0300

    Se crea un helper para manejar los accesos a archivos, y se hace una llamada AJAX para modificar el ID de cliente y el token correspondiente

--- Demo GitHub/Controllers/JsonController.cs
+++ Demo GitHub/Controllers/JsonController.cs
@@ -1,6 +1,5 @@
-﻿using Demo_GitHub.Models;
-using System.IO;
-using System.Text;
+﻿using Demo_GitHub.Helpers;
+using Demo_GitHub.Models;
 using System.Web.Mvc;
 
 namespace Demo_GitHub.Controllers
@@ -16,36 +15,7 @@ namespace Demo_GitHub.Controllers
 
         public void MarkAsReaded(long idCliente, long idComprobante)
         {
-            DeleteLineWithParams(idCliente, idComprobante);
-        }
-
-        //gcozzi@axoft.com: refactorizar, posiblemente extraer clase
-        private static void DeleteLineWithParams(long idCliente, long idComprobante)
-        {
-            string fileName = @"C:\inetpub\wwwroot\Demo GitHub\IO\Comprobantes.txt";
-            string lineToDelete = $"{idCliente};{idComprobante}";
-
-            StringBuilder sb = new StringBuilder();
-            string[] lines = System.IO.File.ReadAllLines(fileName);
-            foreach (string line in lines)
-            {
-                if (line != lineToDelete)
-                {
-                    sb.Append(line);
-                }
-            }
-
-            if (System.IO.File.Exists(fileName))
-                System.IO.File.Delete(fileName);
-
-            using (FileStream fs = new FileStream(fileName, FileMode.OpenOrCreate, FileAccess.Write, FileShare.Write))
-            {
-                StreamWriter sw = new StreamWriter(fs);
-                sw.BaseStream.Seek(0, SeekOrigin.Begin);
-                sw.Write(sb.ToString());
-                sw.Flush();
-                sw.Close();
-            }
-        }
+            FileHelper.DeleteLine(@"C:\inetpub\wwwroot\Demo GitHub\IO\Comprobantes.txt", $"{idCliente};{idComprobante}");
+        }        
     }
 }
---
+++ Demo GitHub/Helpers/FileHelper.cs
@@ -0,0 +1,67 @@
+﻿using System.IO;
+using System.Text;
+
+namespace Demo_GitHub.Helpers
+{
+    public static class FileHelper
+    {
+        /// <summary>
+        /// Borra las líneas de un archivo, cuyo contenido coincide con el indicado
+        /// </summary>
+        /// <param name="filename">Ruta del archivo</param>
+        /// <param name="lineToDelete">Contenido de las lineas a eliminar</param>
+        public static void DeleteLine(string filename, string lineToDelete)
+        {
+            StringBuilder sb = new StringBuilder();
+            string[] lines = File.ReadAllLines(filename);
+
+            foreach (string line in lines)
+            {
+                if (line != lineToDelete)
+                    sb.Append(line);
+            }
+
+            RecreateFile(filename, sb);
+        }
+
+        /// <summary>
+        /// Reemplaza las líneas de un archivo, cuyo contenido coincide con el indicado.
+        /// </summary>
+        /// <param name="filename">Ruta del archivo</param>
+        /// <param name="lineToReplace">Contenido a reemplazar</param>
+        /// <param name="newLineContent">Nuevo contenido de la línea</param>
+        public static void ReplaceLine(string filename, string lineToReplace, string newLineContent)
+        {
+            StringBuilder sb = new StringBuilder();
+            string[] lines = File.ReadAllLines(filename);
+
+            foreach (string line in lines)
+            {
+                string lineToAppend = (line != lineToReplace) ? line : newLineContent;
+                sb.AppendLine(lineToAppend);
+            }
+
+            RecreateFile(filename, sb);
+        }
+
+        /// <summary>
+        /// Elimina un archivo de texto y lo vuele a crear
+        /// </summary>
+        /// <param name="filename">Ruta del archivo de texto a recrear</param>
+        /// <param name="sb">Contenido del archivo de texto</param>
+        private static void RecreateFile(string filename, StringBuilder sb)
+        {
+            if (File.Exists(filename))
+                File.Delete(filename);
+
+            using (FileStream fs = new FileStream(filename, FileMode.OpenOrCreate, FileAccess.Write, FileShare.Write))
+            {
+                StreamWriter sw = new StreamWriter(fs);
+                sw.BaseStream.Seek(0, SeekOrigin.Begin);
+                sw.Write(sb.ToString());
+                sw.Flush();
+                sw.Close();
+            }
+        }
+    }
+}
